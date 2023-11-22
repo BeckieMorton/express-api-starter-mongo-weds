b107b23b889cc10555dec9ce50caff0154610e7e
BeckieMorton <beckam11@gmail.com>
2023-11-22 14:00:54 +0100

    codealong up to 5mins same code as Damien and still crashes

--- server.js
+++ server.js
@@ -1,5 +1,23 @@
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/books";
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = Promise;
+
+const Author = mongoose.model("Author", {
+  name: String,
+});
+
+const Book = mongoose.model("Book", {
+  title: String,
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Author",
+  },
+});
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
@@ -9,11 +27,16 @@ const app = express();
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
-app.use(express.json());
+app.use(bodyParser.json());
 
 // Start defining your routes here
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
+});
+
+app.get("/authors", async (req, res) => {
+  const authors = await Author.find();
+  res.json(authors);
 });
 
 // Start the server
